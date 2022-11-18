ebf29c61a843adc94cdf82efb38ac15938fe1520
Gus <82774618+GooseterV@users.noreply.github.com>
2022-11-18 16:02:48 -0500

    update

--- src/numeral.js
+++ src/numeral.js
@@ -3,21 +3,27 @@ function log(n, base) {
 	return Math.log(n) / Math.log(base);
 }
 
+
+function convert(n, b) {
+	let q = [];
+	let r = [];
+	q[0] = n;
+	for (let i = 0; q[i]>0; i++) {
+		r[i] = q[i]%b;
+		q[i+1] = Math.floor(q[i]/b);
+	}
+	return r.reverse();
+}
+
 class Num {
 
 	constructor(value, base, mappings) {
 		this.value = value;
 		this.base = base;
 		this.mappings = mappings;
+		this.total = 0;
 		this.exp = log(this.value, this.base);
 		this.isEvenBase = Number.isInteger(this.exp);
-	}
-	
-	calc(stack = this.stack, total = this.total) {
-		for (let i = 0; i<stack.length; i++) {
-			total += stack[i].map((x) => this.mappings[x]).reduce((a, b) => a + b) * (this.base**i);
-		}
-		return total;
 	}
 
 }
@@ -40,7 +46,15 @@ class MayanNumeral extends Num {
 		if (value < 1) return null;
 		if (value < 5) return Array(value).fill(1);
 		if (value === 5) return [2];
-		if (value < 20) return (Array(Math.floor(value/5)).fill(2).concat(Array(value%5).fill(1))).reverse();
+		if (value < 20) return (Array(Math.floor(value/5)).fill(5).concat(Array(value%5).fill(1))).reverse();
+	}
+
+	calc(stack = this.stack) {
+		let total = 0;
+		for (let i = 0; i<stack.length; i++) {
+			total += stack[i].map((x) => this.mappings[x]).reduce((a, b) => a + b) * (this.base**i);
+		}
+		return total;
 	}
 
 	export() {
@@ -50,3 +64,49 @@ class MayanNumeral extends Num {
 	}
 }
 
+class BabylonianNumeral extends Num {
+
+	static chars = {
+		0: "␣",
+		1: "𒐕",
+		2: "𒐖",
+		3: "𒐗",
+		4: "𒐘",
+		5: "𒐙",
+		6: "𒐚",
+		7: "𒐛",
+		8: "𒐜",
+		9: "𒐝",
+		10: "𒌋",
+		20: "《",
+		30: "𒌍",
+		40: "𒐏",
+		50: "𒐐",	
+	};
+
+	constructor(value) {
+		super(value, 60, BabylonianNumeral.mappings);
+		this.stack = convert(value, 60).map(x=>this.construct(x));
+	}
+
+	construct(value) {
+		if (value === 0) return [0];
+		if (value < 10) return [value];
+		if (value === 50) return [50];
+		if (value === 40) return [40];
+		if (value === 30) return [30];
+		if (value === 20) return [20];
+		return [parseInt(value.toString()[0]), parseInt(value.toString()[1])];
+	}
+
+	calc() {
+		let stack = [].concat(this.stack);
+		stack.reverse();
+		let total = 0;
+		for (let i = 0; i<stack.length; i++) {
+			total += (parseInt(stack[i].join("")) * (this.base**i));
+		}
+		return total;
+	}
+
+}
